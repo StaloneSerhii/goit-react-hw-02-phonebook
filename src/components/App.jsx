0904020bfdde46e2d6384a962b8cc8215d8eadcd
StaloneSerhii <sergio.fly4405@gmail.com>
2023-02-26 22:00:06 +0200

    dsadsa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ import shortid from 'shortid';
 export class App extends Component {
   state = {
     contacts: [
-      {id:shortid.generate(),  name: 'Rosie Simpson', number: '459-12-56' },
-      {id:shortid.generate(),  name: 'Hermione Kline', number: '443-89-12' },
-      {id:shortid.generate(),  name: 'Eden Clements', number: '645-17-79' },
-      {id:shortid.generate(),  name: 'Annie Copeland', number: '227-91-26' },
+      { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
+      { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     name: '',
     number: '',
@@ -18,39 +18,50 @@ export class App extends Component {
   };
 
   addNamesContact = (name, number) => {
-
     const addCont = {
       id: shortid.generate(),
       name,
       number,
+    };
+
+    const gmapCont = this.state.contacts.map(({ name }) => name.toLowerCase());
+    const nameToLowerCase = name.toLowerCase();
+    if (gmapCont.includes(nameToLowerCase)) {
+      return alert(`${name} is already in contacs.`);
     }
+
     this.setState(prevState => ({
-      contacts: [addCont, ...prevState.contacts]
-    }))  
+      contacts: [addCont, ...prevState.contacts],
+    }));
   };
 
-  filtrChange = (e) => {
-    this.setState({filter: e.target.value})
-  }
+  filtrChange = e => {
+    this.setState({ filter: e.target.value });
+  };
 
-  filterRender= ()=> {
-    const {filter,contacts} = this.state
-    const normalizedFilter = filter.toLocaleLowerCase()
-    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizedFilter))
-  }
+  filterRender = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
 
-onDelete = (id) => {
-console.log(id);
-}
+  onDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
 
+    this.setState({ filter: '' });
+  };
 
   render() {
-const visibleStat=this.filterRender()
+    const visibleStat = this.filterRender();
     return (
       <div>
         <FormContact addNamesContact={this.addNamesContact} />
-        <Filter onChange={this.filtrChange} value={this.state.filter}/>
-        <Contact dataContact={visibleStat } onDelete={this.onDelete}/>
+        <Filter onChange={this.filtrChange} value={this.state.filter} />
+        <Contact dataContact={visibleStat} onDelete={this.onDelete} />
       </div>
     );
   }
